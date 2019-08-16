f0399655c695170256d4967bcf6e7751582cbd84
theProgrammerDavid <davidvelho2awesome@gmail.com>
2019-08-16 19:54:34 +0530

    innitial hbs commit

--- models/leaderboard.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var leaderboard = new Schema({
-	username: {type: String, required: true},
-	points: { type: Number, default: 0 }	
-});
-
-module.exports = mongoose.model('leaderboard', leaderboard);
