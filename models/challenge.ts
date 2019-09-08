47d9ccb559177c439985fc3eec3962560c6f0dc7
Saurav <sauravhiremath@gmail.com>
2019-09-08 20:55:25 +0530

    Flag Submission functions added

--- models/challenge.ts
+++ models/challenge.ts
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const challengeSchema = new Schema(
-  {
-    index: Number,
+const challengeSchema = new Schema({
+    // index: Number,
     name: String,
     description: String,
     hint: { type: String, default: null },
     answer: String,
     startPoints: Number,
-    solved: Array,
-    hidden: Boolean
-  },
-);
+    currentPoints: Number,
+    solvedBy: Array,
+    hidden: { type: Boolean, required: false }
+});
 
 export const Challenge = mongoose.model("Challenge", challengeSchema);
 export default Challenge;
--- routes/handleSubmission.ts
+++ routes/handleSubmission.ts
@@ -3,12 +3,35 @@ import * as mongoose from "mongoose";
 import { Challenge } from "../models/challenge";
 
 export async function handleSubmission(data: submissionData) {
-  console.log(data);
-  const question = await Challenge.findOne({ _id: data.qid });
+    console.log(data);
+    const question = await Challenge.findOne({ _id: data.qid });
 
-  //   question.id["name"] = "dddd";
-  if (data.ctfFlag == question.id["answer"]) {
-    // await question.id[]
-  }
-  console.log(question);
+    if (data.ctfFlag == question.id["answer"] && verifiedSubmission()) {
+        refreshLeaderboard(data);
+        return "Correct Flag";
+    } else {
+        return "Incorrect Flag";
+    }
+
+    console.log(question);
+}
+
+function verifiedSubmission(): boolean {
+    return true;
+}
+
+function refreshLeaderboard(data: submissionData){
+  let submissionPoints = () => {
+    //Changes in the challenge Model        --> change currentPoints and solvedBy
+    //               solvedChallenges Model --> Add new row with all new values
+    //               user Model             --> changed solved and points
+
+    //Update Question Points with new one
+    //Return the previous points value
+  };
+
+  function UpdateLeaderboardModel(){
+    //Changes in leaderboard Model          --> change username and points
+    //Decide a final way to re-Sort Leaderboard, do (on client side) or (on server Side using redis)
+  };
 }
