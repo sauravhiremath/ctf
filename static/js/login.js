23cd82434296ee97b74a0ecdfa3b697a2bc50ee6
devamtrivedi <devamtrivedi71@gmail.com>
2019-09-17 23:13:13 +0530

    fix: question popup ui

--- static/js/login.js
+++ static/js/login.js
@@ -1,17 +1,15 @@
-const usernameInput = $("input[name='username']");
-const passwordInput = $("input[name='password']");
+const Loginusername = $("input[name='username']");
+const Loginpassword = $("input[name='password']");
 
-const usernameregex = /^[a-zA-Z0-9_`!@#$%^&*]{3,20}$/;
-const passregex = /^[a-zA-Z0-9_!@#$%^&* ]{5,15}$/;
 
-function onSubmit(token) {
+function onLoginSubmit(token) {
     const username = encodeURIComponent(usernameInput.val());
     const password = encodeURIComponent(passwordInput.val());
     const formData = `g-recaptcha-response=${token}&username=${username}&password=${password}`;
-
+    console.log(formData)
     $.ajax({
 		type: "POST",
-		url: "/login",
+		url: "/auth/login",
 		data: formData,
         error: (xhr, status, message) => {
             const error = JSON.parse(xhr.responseText);
@@ -28,33 +26,33 @@ function onSubmit(token) {
     grecaptcha.reset();
 }
 
-(()=>{
-    usernameInput.on("keyup", function () {
-        if (!$(this).val().match(regexname)) {
-            // there is a mismatch, hence show the error message
-            $("#nameError.emsg").removeClass("hidden");
-            $("#loginSubmitBtn").prop("disabled", true);
-            $("#nameError.emsg").show();
-        } else {
-            // else, do not display message
-            $("#nameError.emsg").addClass("hidden");
-            $("#loginSubmitBtn").prop("disabled", false);
-        }
-    });
-    passwordInput.on("keyup", function () {
-        if (
-            !$(this)
-                .val()
-                .match(passregex)
-        ) {
-            // there is a mismatch, hence show the error message
-            $("#passwordError.emsg").removeClass("hidden");
-            $("#loginSubmitBtn").prop("disabled", true);
-            $("#passwordError.emsg").show();
-        } else {
-            // else, do not display message
-            $("#passwordError.emsg").addClass("hidden");
-            $("#loginSubmitBtn").prop("disabled", false);
-        }
-    });
-})
+
+usernameInput.on("keyup", function () {
+    if (!$(this).val().match(regexname)) {
+        // there is a mismatch, hence show the error message
+        $("#nameError.emsg").removeClass("hidden");
+        $("#loginSubmitBtn").prop("disabled", true);
+        $("#nameError.emsg").show();
+        console.log("aaaaaa");
+    } else {
+        // else, do not display message
+        $("#nameError.emsg").addClass("hidden");
+        $("#loginSubmitBtn").prop("disabled", false);
+    }
+});
+passwordInput.on("keyup", function () {
+    if (
+        !$(this)
+            .val()
+            .match(passregex)
+    ) {
+        // there is a mismatch, hence show the error message
+        $("#passwordError.emsg").removeClass("hidden");
+        $("#loginSubmitBtn").prop("disabled", true);
+        $("#passwordError.emsg").show();
+    } else {
+        // else, do not display message
+        $("#passwordError.emsg").addClass("hidden");
+        $("#loginSubmitBtn").prop("disabled", false);
+    }
+});
