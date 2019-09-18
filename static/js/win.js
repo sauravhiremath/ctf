078c099ac465949bf20c33200392e70ca4a6c203
theProgrammerDavid <davidvelho2awesome@gmail.com>
2019-09-18 22:30:48 +0530

    basic leaderboard and buttons

--- static/js/win.js
+++ static/js/win.js
@@ -1,8 +1,10 @@
 document.getElementsByTagName("img").draggable = false;
 console.log("hello")
 
-$(document).on("click", "#leaderBtn", function(){
+$(document).on("click", "#leaderBtn", function (e) {
+
     $("#leaderBoardPopup").modal({
+
         show: true,
         backdrop: false
     }).draggable({
@@ -12,14 +14,26 @@ $(document).on("click", "#leaderBtn", function(){
 
 
 
-$(document).on("click", ".closeIcon", function(){
+$(document).on("click", ".closeIcon", function () {
     $('#questionPopupModal').modal('hide')
 })
-$(document).on("click", ".closebtn", function(){
-   $('#singlePopupModal').modal('hide')
+$(document).on("click", ".closebtn", function () {
+    $('#singlePopupModal').modal('hide')
 })
-$(document).on("click", ".closeQuestion", function(){
+$(document).on("click", ".closeQuestion", function () {
     $('#singlePopupModal').modal('hide')
 })
 
+$(function () {
+    $(".tmngr_btn").click(function () {
+        $(".tmngr_btn").removeClass("active");
+        console.log(this);
+        $(this).addClass("active");
+    });
+});
 
+
+$(document).on("click", ".tmngr_btn", function () {
+
+    //$("#leaderboard_names").addClass("active");
+});
