095a9401710aa470f0eed8a76f31d4005bb9329e
thebongy <rishit.bansal0@gmail.com>
2019-09-19 23:41:19 +0530

    feat: resetPassword

--- static/js/resetpsw.js
+++ static/js/resetpsw.js
@@ -1,3 +1,21 @@
-$(document).on("click", ".reset-psw", function(){
-    
-})
+$("#forgotPasswordBtn").click(() => {
+    const email = encodeURIComponent($("input[name='forgotPassword-email']").val());
+	const formData = `email=${email}`;
+    $.ajax({
+		type: "POST",
+		url: "/auth/resetPassword",
+		data: formData,
+		success: data => {
+			if (data["success"] == true) {
+				$("#resetPasswordForm").hide();
+				$("#postResetContent").show();
+			} else {
+				alert(data["message"]);
+			}
+		},
+		error: (xhr, status, message) => {
+			const error = JSON.parse(xhr.responseText);
+			alert(error);
+		}
+	});
+});
