3a4423d1d8c9b3c2185f57159f70c0797d36ecca
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-09-20 16:19:08 +0530

    prepare to modify gambler

--- scripts/addQuestions.ts
+++ scripts/addQuestions.ts
@@ -11,42 +11,42 @@ var baby = [
     //     solvedBy: [],
     //     hidden: false
     // },
-    // {
-    //     name: "Gambler",
-    //     description: "What happens if we use 100% of our brain!",
-    //     type: "Miscellaneous",
-    //     difficulty: "baby",
-    //     hint: "",
-    //     answer: "CSICTF{Im_N0t_Th4t_Dum8_Aft3r4ll}",
-    //     startPoints: 70,
-    //     currentPoints: 70,
-    //     solvedBy: [],
-    //     hidden: false
-    // },
     {
-		name: "Windows Registry Editor",
-		description: "I was hungry so I made alphabet soup.",
-		difficulty: "hard",
-		type: "Reverse Engineering",
-		hint: null,
-		answer: "CSICTF{NOWYOUKNOWNODEJSBOI}",
-		startPoints: 100,
-		currentPoints: 100,
-		solvedBy: [],
-		hidden: false
-	},
-	{
-		name: "MS Word",
-		description: "I choose to have faith, because without that, I have nothing… It’s the only thing that’s keeping me going.",
-		difficulty: "hard",
-		type: "Jail",
-		hint: null,
-		answer: "CSICTF{Y0u_c4N_J41l_4_rev0luT10nary_buT_N0T_th3_r3V0lUT10n}",
-		startPoints: 100,
-		currentPoints: 100,
-		solvedBy: [],
-		hidden: false
-	}
+        name: "Gambler",
+        description: "What happens if we use 100% of our brain!",
+        type: "Miscellaneous",
+        difficulty: "baby",
+        hint: "",
+        answer: "CSICTF{Im_N0t_Th4t_Dum8_Aft3r4ll}",
+        startPoints: 70,
+        currentPoints: 70,
+        solvedBy: [],
+        hidden: false
+    },
+    // {
+	// 	name: "Windows Registry Editor",
+	// 	description: "I was hungry so I made alphabet soup.",
+	// 	difficulty: "hard",
+	// 	type: "Reverse Engineering",
+	// 	hint: null,
+	// 	answer: "CSICTF{NOWYOUKNOWNODEJSBOI}",
+	// 	startPoints: 100,
+	// 	currentPoints: 100,
+	// 	solvedBy: [],
+	// 	hidden: false
+	// },
+	// {
+	// 	name: "MS Word",
+	// 	description: "I choose to have faith, because without that, I have nothing… It’s the only thing that’s keeping me going.",
+	// 	difficulty: "hard",
+	// 	type: "Jail",
+	// 	hint: null,
+	// 	answer: "CSICTF{Y0u_c4N_J41l_4_rev0luT10nary_buT_N0T_th3_r3V0lUT10n}",
+	// 	startPoints: 100,
+	// 	currentPoints: 100,
+	// 	solvedBy: [],
+	// 	hidden: false
+	// }
 ]
 import { Challenge } from "../models/challenge";
 import io from "socket.io-client";
