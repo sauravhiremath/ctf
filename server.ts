295927430c7d3ee78f10ec5a65dcb32cf1013ea4
thebongy <rishit.bansal0@gmail.com>
2019-09-20 21:38:22 +0530

    fix: Remove challenge creation statements

--- server.ts
+++ server.ts
@@ -48,14 +48,6 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-createQuestion(5);
-createQuestion(4)
-createQuestion(3)
-createQuestion(2)
-createQuestion(1)
-createQuestion(0)
-createQuestion(6)
-
 app.use(
 	session({
 		secret: process.env.SECRET_KEY,
