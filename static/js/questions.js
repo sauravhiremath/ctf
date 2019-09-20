e99aba8d7065dd840e52ba540db03145f8ff7849
devamtrivedi <devamtrivedi71@gmail.com>
2019-09-20 15:31:47 +0530

    fix: ui

--- static/js/questions.js
+++ static/js/questions.js
@@ -24,7 +24,7 @@ $(document).on("dblclick", ".desktop-icon", function(){
                 for(var i=0; i<filtered.length; i++){
                     var image = filtered[i].name.toLowerCase().replace(" ", "-");
                     console.log(image);
-                    var question_data = '<button class="btn singlePopup question-icon col-2" value='+ filtered[i].name +' id='+ filtered[i]._id +'> <div class="icon-container"><img src="/static/images/'+ image +'.png" alt=""><span class="question-title">'+ filtered[i].name +'</span></div></button>'
+                    var question_data = '<button class="btn singlePopup question-icon col-4 d-inline-flex" value='+ filtered[i].name +' id='+ filtered[i]._id +'> <div class="icon-container"><img src="/static/images/'+ image +'.png" alt=""><span class="question-title">'+ filtered[i].name +'</span></div></button>'
                     data+=question_data;
                 }
             }
@@ -38,12 +38,14 @@ $(document).on("dblclick", ".desktop-icon", function(){
                         backdrop: false
                     });
                     return;
-                };
-                var data='';
-                $(".question-type").html(difficulty)
-                for(var i=0; i<arr.length; i++){
-                    var question_data = '<button class="btn singlePopup question-icon" value='+ arr[i].name +' id='+ arr[i]._id +' disabled> <div class="icon-container"><img src="/static/images/'+ arr[i].type +'.png" alt=""><span class="question-title">'+ arr[i].name +'</span></div></button>'
-                    data+=question_data;
+                }
+                else{
+                    var data='';
+                    $(".question-type").html(difficulty)
+                    for(var i=0; i<arr.length; i++){
+                        var question_data = '<button class="btn singlePopup question-icon col-4 d-inline-flex" value='+ arr[i].name +' id='+ arr[i]._id +' disabled> <div class="icon-container"><img src="/static/images/'+ image +'.png" alt=""><span class="question-title">'+ arr[i].name +'</span></div></button>'
+                        data+=question_data;
+                    }
                 }
             }
             
@@ -61,68 +63,71 @@ $(document).on("dblclick", ".desktop-icon", function(){
     })
 })
 
-$(document).on("click", ".start-icon-modal", function(){
-    var button = $(this);
-    var difficulty = $(this).attr("id");
-    diff = difficulty.toLowerCase();
-    var solved = $(this).attr("name");
-    console.log(solved);
-    $.ajax({
-        type: "GET",
-        url: "home/questionStatus",
-        data: {
-            "sortKey": "difficulty",
-            "solved": solved
-        },
-        success: function(result){
-            var arr=result["allChallenges"]
-            console.log(arr);
-            //unsolved question
-            if(solved=="False"){
-                $(".question-type").html(difficulty);
-                var filtered = arr.filter(question=>question.difficulty==diff)
-                if(filtered.length === 0) return;
-                console.log(filtered);
-                var data = '';
-                for(var i=0; i<filtered.length; i++){
-                    var image = filtered[i].name.toLowerCase().replace(" ", "-");
-                    console.log(image);
-                    var question_data = '<button class="btn singlePopup question-icon col-2" value='+ filtered[i].name +' id='+ filtered[i]._id +'> <div class="icon-container"><img src="/static/images/'+ image +'.png" alt=""><span class="question-title">'+ filtered[i].name +'</span></div></button>'
-                    data+=question_data;
-                }
-            }
-            //solved questions
-            else{
-                if(arr.length === 0){
-                    var message = "No questions solved yet";
-                    $(".message").html(message);
-                    $("#errorModal").modal({
-                        show: true,
-                        backdrop: false
-                    });
-                    return;
-                };
-                var data='';
-                $(".question-type").html(difficulty)
-                for(var i=0; i<arr.length; i++){
-                    var question_data = '<button class="btn singlePopup question-icon" value='+ arr[i].name +' id='+ arr[i]._id +' disabled> <div class="icon-container"><img src="/static/images/'+ arr[i].type +'.png" alt=""><span class="question-title">'+ arr[i].name +'</span></div></button>'
-                    data+=question_data;
-                }
-            }
+// $(document).on("click", ".start-icon-modal", function(){
+//     var button = $(this);
+//     var difficulty = $(this).attr("id");
+//     diff = difficulty.toLowerCase();
+//     var solved = $(this).attr("name");
+//     console.log(solved);
+//     $.ajax({
+//         type: "GET",
+//         url: "home/questionStatus",
+//         data: {
+//             "sortKey": "difficulty",
+//             "solved": solved
+//         },
+//         success: function(result){
+//             var arr=result["allChallenges"]
+//             console.log(arr);
+//             //unsolved question
+//             if(solved=="False"){
+//                 $(".question-type").html(difficulty);
+//                 var filtered = arr.filter(question=>question.difficulty==diff)
+//                 if(filtered.length === 0) return;
+//                 console.log(filtered);
+//                 var data = '';
+//                 for(var i=0; i<filtered.length; i++){
+//                     var image = filtered[i].name.toLowerCase().replace(" ", "-");
+//                     console.log(image);
+//                     var question_data = '<button class="btn singlePopup question-icon col-4 d-inline-flex" value='+ filtered[i].name +' id='+ filtered[i]._id +'> <div class="icon-container"><img src="/static/images/'+ image +'.png" alt=""><span class="question-title">'+ filtered[i].name +'</span></div></button>'
+//                     data+=question_data;
+//                 }
+//             }
+//             //solved questions
+//             else{
+//                 if(arr.length === 0){
+//                     var message = "No questions solved yet";
+//                     $(".message").html(message);
+//                     $("#errorModal").modal({
+//                         show: true,
+//                         backdrop: false
+//                     });
+//                     return;
+//                 };
+//                 var data='';
+//                 $(".question-type").html(difficulty)
+//                 console.log(difficulty);
+//                 for(var i=0; i<arr.length; i++){
+//                     var image = arr[i].name.toLowerCase().replace(" ", "-");
+//                     console.log("asdfsafasdf");
+//                     var question_data = '<button class="btn singlePopup question-icon" value='+ arr[i].name +' id='+ arr[i]._id +' disabled> <div class="icon-container"><img src="/static/images/'+ image +'.png" alt=""><span class="question-title">'+ arr[i].name +'</span></div></button>'
+//                     data+=question_data;
+//                 }
+//             }
             
-            document.getElementById("question-data").innerHTML = data;
-            var cl = $(button).attr("class");
-            cl = cl.split(" ");
-            var id = cl[1];
-            $('#' + id + 'Modal').modal({
-                show: true,
-                backdrop: false
-            }).draggable({
-                handle: ".app_header"
-            })
-        },
-    })
-})
+//             document.getElementById("question-data").innerHTML = data;
+//             var cl = $(button).attr("class");
+//             cl = cl.split(" ");
+//             var id = cl[1];
+//             $('#' + id + 'Modal').modal({
+//                 show: true,
+//                 backdrop: false
+//             }).draggable({
+//                 handle: ".app_header"
+//             })
+//         },
+//     })
+// })
 
 
 $(document).on("dblclick", ".question-icon", function(){
@@ -138,7 +143,7 @@ $(document).on("dblclick", ".question-icon", function(){
                 var data = result["message"]
                 console.log(data.qname);
                 var questionName = data.qname;
-                var questionCatagory = '<div class="d-flex p-2"><strong>Catagory: Web</strong><div class="ml-auto"><strong>Current Points: 100</strong></div></div>'
+                var questionCatagory = '<div class="d-flex p-2"><strong>Catagory: '+ data.type +'</strong><div class="ml-auto"><strong>Current Points: '+ data.currentPoints +'</strong></div></div><div class="d-flex pr-2"><div class="ml-auto"><strong> Start Points: '+ data.startPoints +'</strong></div></div>'
                 var question_text = '<p>'+ data.description +'</p>'
                 var text_field = '<div class="col-10"><input type="text" class="w-100 pl-1" name="flag-input" placeholder="CSICTF{}"></div><div class=" pl-3 w-50"><button class="pl-3 pr-3 submit-button" id='+ data.id + ' data-toggle="modal">Submit</button></div>'
                 var people = data["solvedBy"];
