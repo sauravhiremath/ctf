3860d8ac675a7f8b4bb6caf6244b5b37b11417cb
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-09-20 15:42:36 +0530

    update: Added line

--- server.ts
+++ server.ts
@@ -48,7 +48,7 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-createQuestion();
+//createQuestion();
 
 require("dotenv").config();
 app.use(
